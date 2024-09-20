a120a623410cfb7b48c4aa38f60ceb7690209f8a
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2024-09-20 01:10:20 -0300

    first commit

---
+++ src/main/java/com/Projeto/pi/medioTec/MedioTecApplication.java
@@ -0,0 +1,13 @@
+package com.Projeto.pi.medioTec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedioTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedioTecApplication.class, args);
+	}
+
+}
